b3435cfaa2197338894d34a2ae2a8e39cd3b675a
vlajksanyee <vlajksanyi@gmail.com>
2021-03-07 17:57:56 +0100

    Update #3
    Add Tabcontrol
    Add Members data
    Add Remove feature (alpha)

--- library/library/MainWindow.xaml.cs
+++ library/library/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace library
 {
@@ -24,7 +12,8 @@ namespace library
 		public MainWindow()
 		{
 			InitializeComponent();
-			LoadData("konyvek.txt");
+			LoadBooksData("konyvek.txt");
+			LoadMembersData("tagok.txt");
 		}
 
 		public class Library
@@ -35,7 +24,6 @@ namespace library
 			public string yearOfPublishing { get; set; }
 			public string publisher { get; set; }
 			public bool rentalAvailability { get; set; }
-
 			public Library(string row)
 			{
 				string[] part = row.Split(';');
@@ -48,27 +36,145 @@ namespace library
 			}
 		}
 
-		List<Library> dataList = new List<Library>();
-
-		public void LoadData(string fileName)
+		public class Library2
 		{
+			public int bookID { get; set; }
+			public string author { get; set; }
+			public string bookTitle { get; set; }
+			public string yearOfPublishing { get; set; }
+			public string publisher { get; set; }
+			public bool rentalAvailability { get; set; }
+		}
+
+		//public static List<Library> dataList = new List<Library>();
+
+		public void LoadBooksData(string fileName)
+		{
+			//dataList.Clear();
+			dataGrid.Items.Clear();
 			int i = 0;
 			foreach (var item in File.ReadAllLines(fileName))
 			{
-				dataList.Add(new Library(item));
-				dataGrid.Items.Add(dataList[i]);
+				//dataList.Add(new Library(item));
+				dataGrid.Items.Add(new Library(item));
 				i++;
 			}
 		}
 
-		private void AddBookBT_Click(object sender, RoutedEventArgs e)
+		public class Members
 		{
-			abAuthor.Visibility = Visibility.Visible;
-			abTitle.Visibility = Visibility.Visible;
-			abYear.Visibility = Visibility.Visible;
-			abPublisher.Visibility = Visibility.Visible;
-			abRental.Visibility = Visibility.Visible;
-			abAddBT.Visibility = Visibility.Visible;
+			public int memberID { get; set; }
+			public string memberName { get; set; }
+			public string memberDOB { get; set; }
+			public string memberPC { get; set; }
+			public string memberLoc { get; set; }
+			public Members(string row)
+			{
+				string[] part = row.Split(';');
+				memberID = int.Parse(part[0]);
+				memberName = part[1];
+				memberDOB = part[2];
+				memberPC = part[3];
+				memberLoc = part[4] + " " + part[5];
+			}
+		}
+
+		public class Members2
+		{
+			public int memberID { get; set; }
+			public string memberName { get; set; }
+			public string memberDOB { get; set; }
+			public string memberPC { get; set; }
+			public string memberLoc { get; set; }
+		}
+
+		//public static List<Members> membersDataList = new List<Members>();
+
+		public void LoadMembersData(string fileName)
+		{
+			//membersDataList.Clear();
+			membersDataGrid.Items.Clear();
+			int i = 0;
+			foreach (var item in File.ReadAllLines(fileName))
+			{
+				//membersDataList.Add(new Members(item));
+				membersDataGrid.Items.Add(new Members(item));
+				i++;
+			}
+		}
+
+		private void AbAddBT_Click(object sender, RoutedEventArgs e)
+		{
+			Library2 addBook = new Library2();
+			addBook.bookID = dataGrid.Items.Count + 1;
+			addBook.author = abAuthor.Text;
+			addBook.bookTitle = abTitle.Text;
+			addBook.yearOfPublishing = abYear.Text;
+			addBook.publisher = abPublisher.Text;
+			addBook.rentalAvailability = (bool)abRental.IsChecked;
+			StreamWriter sw = new StreamWriter("konyvek.txt", true);
+			sw.WriteLine("{0};{1};{2};{3};{4};{5}", addBook.bookID, addBook.author, addBook.bookTitle, addBook.yearOfPublishing, addBook.publisher, addBook.rentalAvailability);
+			sw.Close();
+			LoadBooksData("konyvek.txt");
+		}
+
+		private void RemoveBT_Click(object sender, RoutedEventArgs e)
+		{
+			var grid = dataGrid;
+			var myGrid = dataGrid;
+			if (grid.SelectedIndex >= 0)
+			{
+				for (int i = 0; i <= grid.SelectedItems.Count; i++)
+				{
+					myGrid.Items.Remove(grid.SelectedItems[i]);
+				}
+
+				//foreach (var item in dataList)
+				//{
+				//	int saveID = item.bookID;
+				//	string saveAuthor = item.author;
+				//	string saveTitle = item.bookTitle;
+				//	string saveYear = item.yearOfPublishing;
+				//	string savePublisher = item.publisher;
+				//	bool saveRental = item.rentalAvailability;
+				//	sw.WriteLine("{0};{1};{2};{3};{4};{5}", saveID, saveAuthor, saveTitle, saveYear, savePublisher, saveRental);
+				//}
+				//LoadBooksData("konyvek.txt");
+			}
+			grid = myGrid;
+		}
+
+		private void SearchTB_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+		{
+
+		}
+
+		private void AddMemberBT_Click(object sender, RoutedEventArgs e)
+		{
+			Members2 addMember = new Members2();
+			addMember.memberID = membersDataGrid.Items.Count + 1;
+			addMember.memberName = amName.Text;
+			addMember.memberDOB = amDOB.Text;
+			addMember.memberPC = amPC.Text;
+			addMember.memberLoc = amLoc.Text;
+			StreamWriter sw = new StreamWriter("tagok.txt", true);
+			sw.WriteLine("{0};{1};{2};{3};{4}", addMember.memberID, addMember.memberName, addMember.memberDOB, addMember.memberPC, addMember.memberLoc);
+			sw.Close();
+			LoadMembersData("tagok.txt");
+		}
+
+		private void RemoveMemberBT_Click(object sender, RoutedEventArgs e)
+		{
+			var grid = membersDataGrid;
+			var myGrid = membersDataGrid;
+			if (grid.SelectedIndex >= 0)
+			{
+				for (int i = 0; i <= grid.SelectedItems.Count; i++)
+				{
+					myGrid.Items.Remove(grid.SelectedItems[i]);
+				}
+			}
+			grid = myGrid;
 		}
 	}
 }
